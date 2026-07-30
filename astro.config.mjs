d069093976142e5189fff9e37cb7e8c3a3549815
Zahan <100759044+Zahanturel@users.noreply.github.com>
2026-07-30 16:56:14 +0800

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://zahanturel.github.io',
-  base: '/dadarathornaninstitute',
-  integrations: [tailwind()],
-  output: 'static',
-  redirects: {
-    '/help': '/donate',
-  },
+    site: 'https://dadarathornaninstitute.org',
+    integrations: [tailwind()],
+    output: 'static',
+    redirects: {
+          '/help': '/donate',
+    },
 });
